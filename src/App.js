e78c9df9972578cbb3add64b6b01b2a04cc9d3a8
Abboudy Gogo <abboudygogo@outlook.com>
2020-07-22 16:10:23 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -31,8 +31,7 @@ const App = () => {
 
   useEffect(
     ()=>{
-        // when (and if) globalState.loggedIn changes,
-        // run the below code
+      
         console.log("This the loggedIn state", globalState.loggedIn)
     }, 
     [globalState.loggedIn]
